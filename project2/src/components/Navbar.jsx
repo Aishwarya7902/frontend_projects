84cacebf9cab0b1dad5f29314e63d34f6a501e06
Aishwarya7902 <striver.aish10j20a9720@gmail.com>
2025-06-30 23:05:40 +0530

    showing different categories and respective color in nav

--- project2/src/components/Navbar.jsx
+++ project2/src/components/Navbar.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { productContext } from '../utils/Context'
+import { Link } from 'react-router-dom'
+
 
 const Navbar = () => {
+  const [products] =useContext(productContext)
+
+  let distinctCat= products && products.reduce((acc,cv) => [...acc,cv.category],[])
+  distinctCat = [... new Set(distinctCat)]
+   
+  const color = () => {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgba(${r},${g},${b},0.4)`;
+};
+  
+
   return (
     <nav className="h-full w-[15%] bg-zinc-100 flex flex-col items-center p-5">
         <a
@@ -11,27 +27,14 @@ const Navbar = () => {
         </a>
         <hr className="w-[80%] my-3" />
         <h1 className="text-2xl w-[80%] mb-3">Category</h1>
-        <ul className=" w-[80%]">
-          <li className=" mb-3 flex items-center">
-            <span className="h-[15px] w-[15px] rounded-full  bg-blue-100 mr-5"></span>
-            Category1
-          </li>
+        <div className=" w-[80%]">
+          {distinctCat.map((cat,i)=><Link key={i} to={`/?category=${cat}`} className=" mb-3 flex items-center">
+            <span style={{backgroundColor:color()}} className="h-[15px] w-[15px] rounded-full   mr-2"></span>
+              {cat}
+          </Link>)}
 
-          <li className=" mb-3 flex items-center">
-            <span className="h-[15px] w-[15px] rounded-full  bg-red-100 mr-5"></span>
-            Category2
-          </li>
-
-          <li className=" mb-3 flex items-center">
-            <span className="h-[15px] w-[15px] rounded-full  bg-yellow-200 mr-5"></span>
-            Category3
-          </li>
-
-          <li className=" mb-3 flex items-center">
-            <span className="h-[15px] w-[15px] rounded-full  bg-purple-300 mr-5"></span>
-            Category4
-          </li>
-        </ul>
+          
+        </div>
       </nav>
   )
 }
