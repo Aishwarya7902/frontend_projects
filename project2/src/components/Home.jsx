08dae62e1552eff837f2403f00d8334acbe427df
Aishwarya7902 <striver.aish10j20a9720@gmail.com>
2025-07-02 12:18:45 +0530

    organizing products via category

--- project2/src/components/Home.jsx
+++ project2/src/components/Home.jsx
@@ -1,29 +1,58 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Navbar from "./Navbar";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { productContext } from "../utils/Context";
 import Loading from "./Loading";
+import axios from "../utils/axios";
 
 const Home = () => {
+  const [products] = useContext(productContext);
+  const [filteredProducts,setFilteredProducts]=useState(null)
 
- const [products] =useContext(productContext)
- 
-  return  products ? ( 
+  const { search } = useLocation();
+  const category = decodeURIComponent(search.split("=")[1]);
+  
+
+  const getProductsCategory = async () => {
+    try {
+      const { data } = await axios.get(`/products/category/${category}`);
+      setFilteredProducts(data);
+
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if(!filteredProducts)setFilteredProducts(products)
+    if (category !="undefined") getProductsCategory();
+  }, [category,products]);
+
+  return products ? (
     <>
-      <Navbar/>
-      <div className="h-full w-[85%] flex flex-wrap overflow-x-hidden overflow-y-auto">
+      <Navbar />
 
-        {products.map((p,i)=><Link to={`/details/${p.id}`} className=" m-5 h-[45vh] w-[20%] card p-2 pt-0 border shadow rounded flex flex-col justify-center">
-          <div className="w-full h-[80%] bg-contain bg-no-repeat bg-center" style={{
-            backgroundImage:`url(${p.image})`
-          }}>
-            
-          </div>
-          <h1>{p.title}</h1>
-        </Link>)}
+      
+      <div className="h-full w-[85%] flex flex-wrap overflow-x-hidden overflow-y-auto">
+        {filteredProducts && filteredProducts.map((p, i) => (
+          <Link
+            to={`/details/${p.id}`}
+            className=" m-5 h-[45vh] w-[20%] card p-2 pt-0 border shadow rounded flex flex-col justify-center"
+          >
+            <div
+              className="w-full h-[80%] bg-contain bg-no-repeat bg-center"
+              style={{
+                backgroundImage: `url(${p.image})`,
+              }}
+            ></div>
+            <h1>{p.title}</h1>
+          </Link>
+        ))}
       </div>
-    </>
-  ) :(<Loading/>);
+   </>
+  ) : (
+    <Loading />
+  );
 };
 
 export default Home;
