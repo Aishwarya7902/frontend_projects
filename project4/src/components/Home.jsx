445854c9046d4c57561ae13500154ba6868ea6b5
Aishwarya7902 <striver.aish10j20a9720@gmail.com>
2025-07-19 19:49:03 +0530

    Sidenav component added

---
+++ project4/src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Sidenav from '../partials/Sidenav'
+
+const Home = () => {
+  document.title="CuddleScreen | Homepage"
+  return (
+    <>
+    <Sidenav/>
+    <div className='w-[80%] h-full '></div>
+    
+    </>
+  )
+}
+
+export default Home
---
+++ project4/src/partials/Sidenav.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Sidenav = () => {
+  return (
+    <div className="w-[20%] h-full border-r-2 border-zinc-400 p-10">
+      <h1 className="text-white font-bold ">
+        <i className="text-[#6556CD] ri-tv-fill  mr-3 text-2xl "></i>
+        <span className="text-2xl text-purple-200">CuddleScreen</span>
+      </h1>
+
+      <nav className="flex flex-col  text-xl gap-5">
+        <h2 className="text-white font-semibold text-xl mt-10 mb-5">
+          New Feeds
+        </h2>
+        <Link to="/trending" className="!text-zinc-400 hover:!text-white ">
+          <i className=" mr-2 ri-fire-fill"></i>Trending
+        </Link>
+        <Link to="/popular" className="!text-zinc-400 hover:!text-white">
+          <i className=" mr-2 ri-bard-fill"></i> Popular
+        </Link>
+        <Link to="/movies" className="!text-zinc-400 hover:!text-white">
+          <i className="mr-2 ri-movie-2-fill"></i> Movies
+        </Link>
+        <Link to="/tv-shows" className="!text-zinc-400 hover:!text-white">
+          <i className="mr-2 ri-tv-2-fill"></i>TV Shows
+        </Link>
+        <Link to="/people" className="!text-zinc-400 hover:!text-white">
+          <i className="mr-2 ri-team-line"></i>People
+        </Link>
+      </nav>
+      
+       
+      
+    </div>
+  );
+};
+
+export default Sidenav;
