560cf927dd6f74aa787c990b0689854eb9a609f5
Aishwarya7902 <striver.aish10j20a9720@gmail.com>
2025-07-23 16:42:02 +0530

    fixed search functionality in topnav

--- project4/src/partials/Topnav.jsx
+++ project4/src/partials/Topnav.jsx
@@ -21,17 +21,19 @@ const Topnav = () => {
       getSearches()
     },[query])
   return (
-    <div className='h-[12vh] w-full  relative flex justify-start items-center ml-[20%]'>
+    <div className='h-[12vh] w-[80%]  relative flex  items-center mx-auto'>
+
           <i className="text-2xl text-zinc-400 ri-search-2-line"></i>
           <input 
-          className='w-[50%] text-xl mx-10 p-2 outline-none border-none bg-transparent text-zinc-200' type="text" 
+          className=' w-[50%]  text-xl mx-10 p-2 outline-none border-none  text-zinc-200' type="text" 
           placeholder='search anything'
           value={query}
           onChange={(e)=>setQuery(e.target.value)}
           />
-          {query.length>0 && <i  onClick={()=>setQuery("")} className="text-3xl text-zinc-400 ri-close-line cursor-pointer"></i>}
 
-          <div className='w-[50%] max-h-[50vh] absolute bg-zinc-200 left-[5%] top-[90%] overflow-auto rounded'>
+          {query.length>0 && <i  onClick={()=>setQuery("")} className=" text-3xl text-zinc-400 ri-close-line cursor-pointer "></i>}
+
+          <div className='w-[60%]  max-h-[50vh] absolute bg-zinc-200 left-[5%] top-[90%] overflow-auto rounded'>
                   {searches.map((s,i)=>(<Link key={i} to="/" className='hover:!text-black font-semibold !text-zinc-600 flex justify-start items-center w-[100%] p-10  border-b-2 border-zinc-100' >
                     <img className='w-[10vh] h-[10vh] object-cover rounded mr-5' 
                     src={s.backdrop_path || s.profile_path ? `https://image.tmdb.org/t/p/original/${s.backdrop_path || s.profile_path}`:noimage} 
