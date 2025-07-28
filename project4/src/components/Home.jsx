74e6577f3180aecacc81919bc5a405b9735632fc
Aishwarya7902 <striver.aish10j20a9720@gmail.com>
2025-07-28 13:06:04 +0530

    added filter functionality on horizontal card component

--- project4/src/components/Home.jsx
+++ project4/src/components/Home.jsx
@@ -4,11 +4,14 @@ import Topnav from "../partials/Topnav";
 import axios from "../utils/Axios";
 import Header from "../partials/Header";
 import HorizontalCards from "../partials/HorizontalCards";
+import Dropdown from "../partials/Dropdown";
 
 const Home = () => {
   document.title = "CuddleScreen | Homepage";
   const [wallpaper, setWallpaper] = useState(null);
   const [trending,setTrending]=useState(null);
+  const [category,setCategory] =useState('all');
+
 
   const getHeaderWallpaper = async () => {
     try {
@@ -23,13 +26,14 @@ const Home = () => {
   console.log(wallpaper);
   useEffect(() => {
     !wallpaper && getHeaderWallpaper();
-    !trending && getTrending();
-  }, []);
+    getTrending();
+  }, [category]);
   console.log(trending)
+
 
   const getTrending = async () => {
     try {
-      const { data } = await axios.get(`/trending/all/day`);
+      const { data } = await axios.get(`/trending/${category}/day`);
       setTrending(data.results);
     } catch (error) {
       console.log("error", error);
@@ -42,7 +46,11 @@ const Home = () => {
       <div className="w-[80%] min-h-full overflow-auto overflow-x-hidden">
         <Topnav />
         <Header data={wallpaper} />
-        <HorizontalCards data={trending}/>
+        <div className="p-5 flex justify-between">
+                <h1 className=" text-xl font-semibold mb-1">Trending</h1>
+                <Dropdown title='Filter' options={['tv','movie','all']} func={(e)=>setCategory(e.target.value)}/>
+              </div>
+        <HorizontalCards data={trending} />
       </div>
 
     </>
